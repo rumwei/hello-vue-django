62abd8555f3876016f74b04488122ce90290f208
rumwei <isguwei@163.com>
2019-01-20 23:19:12 +0800

    注释解释

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import ckEditor from './ckEditor.vue'
 
+// 以下的eslint-disable-line注释是为了禁止ESLint进行检测的，因为在js中，new一个对象必须将该对象赋值给一个变量，
+// 而vue的实例化是不需要的，因此为了避免报错(实际没错)添加了该注解
+
 // eslint-disable-next-line no-new
 new Vue({
     el: '#app',
